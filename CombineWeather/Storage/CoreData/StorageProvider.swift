f6bc41b712da2fd37d9e7d98316cda3eb2acf0c4
Andrii Durbalo <andrey.durbalo@gmail.com>
2021-02-10 18:23:06 +0200

    Add base CoreData stack

---
+++ CombineWeather/Storage/CoreData/StorageProvider.swift
@@ -0,0 +1,53 @@
+//
+//  StorageProvider.swift
+//  CombineWeather
+//
+//  Created by Andrii Durbalo on 10.02.2021.
+//
+
+import Foundation
+import CoreData
+
+class StorageProvider {
+    
+    let persistentContainer: NSPersistentContainer
+    
+    init() {
+        
+        persistentContainer = NSPersistentContainer(name: "Storage")
+        persistentContainer.loadPersistentStores { (desc, error) in
+            
+            if let error = error {
+                fatalError("error: \(error.localizedDescription)")
+            }
+            print(desc)
+        }
+    }
+}
+
+
+extension StorageProvider {
+    
+    func save(city: CitiesListViewController.City) {
+        
+        let newCity = MOCity(context: persistentContainer.viewContext)
+        newCity.id = Int64(city.id)!
+        newCity.name = city.name
+        newCity.country = "UA"
+        newCity.state = ""
+        
+        let coord = MOCoordinate(context: persistentContainer.viewContext)
+        coord.lat = 46.477474
+        coord.lon = 30.732622
+            
+        newCity.coord = coord
+        
+        do {
+            try persistentContainer.viewContext.save()
+            print("Saved")
+        } catch {
+            print("failer: \(error)")
+        }
+        
+    }
+}
